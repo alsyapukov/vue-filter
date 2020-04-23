62ed6713a3602a0aef961ad63ba56696bc16727d
megacracker <al.syapukov@gmail.com>
2020-04-23 12:35:17 +0400

    change path

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
         name: path.join('../src/assets/', 'img/[name].[ext]')
       })
   },
-  publicPath: '/',
+  publicPath: '/vue-filter/',
   productionSourceMap: true,
   assetsDir: './assets'
 }
